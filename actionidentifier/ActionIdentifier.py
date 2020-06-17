1c12c360aac4ad1fd3ef9d446126b3d34b109ec6
Mauricio Steinert <mauricio.steinert@edu.pucrs.br>
2020-06-17 20:40:19 -0300

    Action identifier compute true positive values

--- actionidentifier/ActionIdentifier.py
+++ actionidentifier/ActionIdentifier.py
@@ -28,6 +28,10 @@ class ActionIdentifier():
             for sentence in text:
                 # Tokenize
                 sentence_tokens = nltk.word_tokenize(sentence)
+
+                if self.config['action_identifier']['add_pronoun']:
+                    sentence_tokens.insert(0, 'they')
+
                 sentence_tags = nltk.pos_tag(sentence_tokens)
 
                 utils.write_log(self.config, "\n>SENTENCE: {}".format(sentence))
@@ -35,20 +39,28 @@ class ActionIdentifier():
                 nltk_verbs = [v[0] for v in sentence_tags if v[1] in ['VB', 'VBZ', 'VBP', 'VBG']]
                 utils.write_log(self.config, "\n  >NLTK VERBS: {}".format(nltk_verbs))
 
+                embedding_verbs = []
                 for token, tag in zip(sentence_tokens, sentence_tags):
-                    embedding_verbs = []
                     keyword_similarity = []
                     for keyword in self.config['action_identifier']['keywords']:
-                        d = 1.0 - self.word_embedding.get_distance(token, keyword)[2]
-                        keyword_similarity.append(d)
-                        mean = np.mean(keyword_similarity)
+                        try:
+                            similarity = 1.0 - self.word_embedding.get_distance(token, keyword)[2]
+                        except KeyError:
+                            similarity = 0.0
 
-                    if mean >= self.config['action_identifier']['similarity_threshold']:
+                        keyword_similarity.append(similarity)
+
+                    mean = np.mean(keyword_similarity)
+
+                    if mean >= float(self.config['action_identifier']['similarity_threshold']):
                         embedding_verbs.append((token, mean))
 
+                    true_positive = [e[0] in nltk_verbs for e in embedding_verbs]
                     sentence_entry = (token, tag, self.word_embedding.get_word_vector(token), keyword_similarity, mean)
-                utils.write_log(self.config, "\n  >EMBEDDING VERBS: {}".format(embedding_verbs))
-                    # print(sentence_entry)
+                utils.write_log(self.config, "\n  >EMBEDDING VERBS: {} - {}".format(embedding_verbs, true_positive))
+
+            # Text statistics [true positive, false negative, mean_distance]
+
 
 
         return None
