24c369bf40139887956fec4b7428f7e73c4b3ee9
Mauricio Steinert <mauricio.steinert@edu.pucrs.br>
2020-06-17 06:14:58 -0300

    Code refactor; adding NLTK tokens to sentences

--- actionidentifier/ActionIdentifier.py
+++ actionidentifier/ActionIdentifier.py
@@ -1,6 +1,7 @@
 from utils import WordEmbedding
 from wikihow import Wikihow
 import numpy as np
+import nltk
 
 class ActionIdentifier():
     def __init__(self, config):
@@ -14,31 +15,50 @@ class ActionIdentifier():
         wikihow = Wikihow.Wikihow(self.config)
         instance = wikihow.process_example(wikihow.get_entry(0))
 
-        print(instance)
+        for sentence in instance:
+            print("Sentence: {}".format(sentence))
+            # Tokenize
+            sentence_tokens = nltk.word_tokenize(sentence)
+            sentence_tags = nltk.pos_tag(sentence_tokens)
+            print("Tokens: {}".format(sentence_tags))
+
+            for token, tag in zip(sentence_tokens, sentence_tags):
+                keyword_similarity = []
+                for keyword in self.config['action_identifier']['keywords']:
+                    d = 1.0 - self.word_embedding.get_distance(token, keyword)[2]
+                    keyword_similarity.append(d)
+
+                sentence_entry = (token, tag, self.word_embedding.get_word_vector(token), keyword_similarity, np.mean(keyword_similarity))
+                print(sentence_entry)
+
+
         # Convert sentences to embedding
-        sentences_embeddings = self.word_embedding.get_sentence_list_embedding(instance)
-        keyword_embeddings = self.word_embedding.get_sentence_list_embedding(self.config['action_identifier']['keywords'])
-
-        distance = []
-
-        for sentence_list in sentences_embeddings:
-            for sentence in sentence_list:
-                # Compute similarity of each word to predefined keywords
-                for word in sentence:
-                    distance_keyword = []
-                    for keyword in keyword_embeddings:
-                        if len(word[1]) == 1:
-                            distance_keyword.append(float('inf'))
-                        else:
-                            # d = np.linalg.norm((keyword[0][0][1], word[1]))
-                            d = 1.0 - self.word_embedding.model.distance(keyword[0][0][0], word[0])
-                            # print(word[0], keyword[0][0][0], d)
-                            distance_keyword.append(d)
-                    distance.append((word[0], distance_keyword))
-
-        for d in distance:
-            print(d)
-            print()
+        # sentences_embeddings = self.word_embedding.get_sentence_list_embedding(instance)
+        # keyword_embeddings = self.word_embedding.get_sentence_list_embedding(self.config['action_identifier']['keywords'])
+        # sentences_tokens = []
+        # distance = []
+        #
+        # for sentence in instance:
+        #     sentences_tokens.append(nltk.pos_tag(nltk.word_tokenize(sentence)))
+        #
+        #
+        # for sentence_list in sentences_embeddings:
+        #     for sentence, sentence_tokens in zip(sentence_list, sentences_tokens):
+        #         # Compute similarity of each word to predefined keywords
+        #         for word, token in zip(sentence, sentence_tokens):
+        #             distance_keyword = []
+        #             for keyword in keyword_embeddings:
+        #                 if len(word[1]) == 1:
+        #                     distance_keyword.append(float('inf'))
+        #                 else:
+        #                     d = 1.0 - self.word_embedding.model.distance(keyword[0][0][0], word[0])
+        #                     distance_keyword.append(d)
+        #
+        #             distance.append((word[0], token, distance_keyword, np.mean(distance_keyword)))
+        #
+        # for d in distance:
+        #     print(d)
+        #     print()
 
         # print("Keywords:")
         # for keyword in keyword_embeddings:
---
+++ utils/WordEmbedding.py
@@ -0,0 +1,45 @@
+from utils import utils
+from gensim.models import KeyedVectors
+import nltk
+
+
+class WordEmbedding:
+    def __init__(self, config):
+        self.model = KeyedVectors.load_word2vec_format(config['word_embedding_dict'],
+                                                        binary=True,
+                                                        unicode_errors='ignore')
+
+
+    def get_word_vector(self, word):
+        try:
+            return (word, self.model.get_vector(word))
+        except KeyError:
+            return (word, [0])
+
+
+    def get_distance(self, word1, word2):
+        try:
+            return (word1, word2, self.model.distance(word1, word2))
+        except KeyError:
+            return (word1, word2, 0)
+
+
+    def get_sentence_embedding(self, sentence):
+        # words = sentence.split(' ')
+        words = nltk.word_tokenize(sentence)
+        sentence_embedding = []
+        for word in words:
+            try:
+                sentence_embedding.append((word, self.model.get_vector(word)))
+            except KeyError:
+                sentence_embedding.append((word, [0]))
+
+        return sentence_embedding
+
+
+    def get_sentence_list_embedding(self, list_sentence):
+        list_embeddings = []
+        for sentence in list_sentence:
+            list_embeddings.append([self.get_sentence_embedding(sentence)])
+
+        return list_embeddings
